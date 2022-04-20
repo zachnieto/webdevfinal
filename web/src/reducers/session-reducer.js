0084bd16c0c7279e6773e01d17130a65d89407ca
johnlam7707 <johnlam7707@gmail.com>
2022-04-20 15:29:20 -0400

    Fixed bug with clearing the user from the session upon logout

--- web/src/reducers/session-reducer.js
+++ web/src/reducers/session-reducer.js
@@ -1,17 +1,19 @@
 const sessionReducer = (state = {}, action) => {
-    console.log(state)
+    console.log(state);
     switch (action.type) {
         case 'SESSION':
             return action.session;
         case 'LOGOUT':
-            return {}
+            // Removes the user from the session, but keeps other information
+            const clearUser = ({user, ...s}) => s;
+            return clearUser(state);
         case 'USER':
             return {
                 ...state,
                 user: action.user
-            }
+            };
         default:
             return state;
     }
-}
+};
 export default sessionReducer;
